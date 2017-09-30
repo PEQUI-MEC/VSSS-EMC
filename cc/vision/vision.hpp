7d8c89975cfd9c228aa26933ea516c6d94e96418
pqvs <pqverysmall@gmail.com>
2017-09-30 14:13:34 -0300

    função declarada mas não usada em vision.hpp removida

--- cc/vision/vision.hpp
+++ cc/vision/vision.hpp
@@ -67,7 +67,6 @@ private:
   void findTags();
   void findElements();
   void pick_a_tag();
-  int isClose(cv::Point base, cv::Point secondary, cv::Vec4f originalDirection);
   int inSphere(Robot * robot, cv::Point secondary);
 
 public:
