1cfa3da56df3e935b0152af37c545ed1c9f13289
Daniel Faleiro <danielfaleirosilva@gmail.com>
2018-08-17 18:57:20 -0300

    Adiciona código base para o novo goleiro

--- cc/Strategy2/Field.h
+++ cc/Strategy2/Field.h
@@ -11,14 +11,21 @@ namespace field {
 	const double field_height = 1.3;
 	const double field_width = 1.7;
 	const double goal_width = 0.1;
+	const double goal_height = 0.4;
+	const double area_width = 0.15;
+	const double area_height = 0.7;
+	const double corner_height = 0.3;
 	// -----------------------------------------------------------------------------------------------------------------
 
 	// Enumera todas as localizações do campo de interesse
 	enum class Location {
 			OurField,
 			TheirField,
+			UpperField,
+			LowerField,
 			OurGoal,
 			TheirGoal,
+			OurBox, // Pequena área
 	};
 	// -----------------------------------------------------------------------------------------------------------------
 
@@ -29,13 +36,28 @@ namespace field {
 
 	namespace our {
 		namespace area {
-			// Adicione aqui as geometrias de interesse da nossa área
+			namespace goalkeeper {
+				Geometry::Line line( {goal_width+area_width/2, 0}, {goal_width+area_width/2, field_height} );
+				Geometry::Point lower_limit( {goal_width+area_width/2, corner_height+(area_height-goal_height)/2} );
+				Geometry::Point upper_limit( {goal_width+area_width/2, lower_limit.x + goal_height} );
+			}
+			namespace upper {
+				Geometry::Point center( {goal_width+area_width/2, area_height+corner_height} );
+			}
+			namespace lower {
+				Geometry::Point center( {goal_width+area_width/2, corner_height} );
+			}
+			namespace front {
+				Geometry::Point center( {goal_width+area_width, field_height/2} );
+			}
 		}
+
 		namespace goal {
 			namespace front {
 				Geometry::Line line( {goal_width, 0.0}, {goal_width, field_height});
 				Geometry::Point center( {goal_width, field_height/2} );
 			}
+
 			namespace back {
 				// Adicione aqui as geometrias de interesse do fundo do nosso gol
 			}
@@ -63,10 +85,17 @@ namespace field {
 				return position.x < center::point.x;
 			case Location::TheirField:
 				return position.x >= center::point.x;
+			case Location::UpperField:
+				return position.y >= center::point.y;
+			case Location::LowerField:
+				return position.y < center::point.y;
 			case Location::OurGoal:
 				return position.x < our::goal::front::center.x;
 			case Location::TheirGoal:
 				return position.x > their::goal::front::center.x;
+			case Location::OurBox:
+				return position.x >= our::goal::front::center.x && position.x <= our::area::front::center.x
+					&& position.y >= our::area::lower::center.y && position.y <= our::area::upper::center.y;
 			default:
 				return false;
 		}
---
+++ cc/Strategy2/Goalkeeper.hpp
@@ -0,0 +1,24 @@
+//
+// Created by daniel on 17/08/18.
+//
+
+#ifndef VSSS_GOALKEEPER_HPP
+#define VSSS_GOALKEEPER_HPP
+
+#include "Robot2.h"
+#include "Geometry/Geometry.h"
+
+class Goalkeeper : Robot2 {
+	private:
+		enum Behavior {
+			Protect_Goal,
+			Spin_Shot,
+		};
+
+		// Beahviors
+		void protect_goal(const Geometry::Point ball, const Geometry::Point ball_est);
+		void spin_shot( Geometry::Point ball);
+
+};
+
+#endif //VSSS_GOALKEEPER_HPP
