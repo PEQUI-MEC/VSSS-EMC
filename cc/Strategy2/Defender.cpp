cc8f499fb9ca68f753068d9fd3cff2c9439501ff
Daniel Silva <danielfaleirosilva@gmail.com>
2018-09-12 16:25:36 -0300

    Corrige o comportamento do defensor (#145)
    + Cria um const OFFSET para o go_to_and_stop() do Robot2
    + Pequenas melhorias no código
    + Defensor segue o y da bola, em vez de seguir a projeção

--- cc/Strategy2/Defender.cpp
+++ cc/Strategy2/Defender.cpp
@@ -4,19 +4,16 @@ using namespace Geometry;
 using namespace field;
 
 void Defender::wait_at_target(Geometry::Point target, Geometry::Point &ball) {
-	if (distance(get_position(), target) > 0.03)
+	if (distance(get_position(), target) > OFFSET)
 		go_to_and_stop(target);
 	else
 		set_target_orientation(ball - get_position());
 }
 
-void Defender::protect_goal(const Geometry::Point &ball, const Geometry::Point &ball_est) {
-	const Line ball_est_line(ball, ball_est);
-	const Point goal_intersection = intersection(our::goal::front::line, ball_est_line);
-
-	if (at_location(goal_intersection, Location::OurBox)) {
-		go_to_and_stop(intersection(defender::back::line, ball_est_line));
-	} else if (at_location(goal_intersection, Location::UpperField)){
+void Defender::protect_goal(const Geometry::Point &ball) {
+	if (ball.x >= get_position().x) {
+		go_to_and_stop({defender::back::upper_limit.x, ball.y});
+	} else if (at_location(ball, Location::UpperField)){
 		go_to_and_stop(defender::back::upper_limit);
 	} else {
 		go_to_and_stop(defender::back::lower_limit);
