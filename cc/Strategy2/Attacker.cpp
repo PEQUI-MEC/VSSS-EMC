e77713aec2503bb2e604f05a40ccf7b3e1c45836
Daniel Silva <danielfaleirosilva@gmail.com>
2018-10-31 09:45:53 -0300

    Corrige os giros do atacante e goleiro (#153)

--- cc/Strategy2/Attacker.cpp
+++ cc/Strategy2/Attacker.cpp
@@ -3,7 +3,20 @@
 using namespace Geometry;
 using namespace field;
 
-void Attacker::uvf_to_goal(Point ball) {
+void Attacker::decide_spin_shot(const Geometry::Point &ball) {
+	double upper_y = get_position().y + size/2;
+	double upper_x = get_position().x + size/2;
+	double lower_x = get_position().x - size/2;
+
+	if ((at_location(get_position(), Location::UpperField) && (ball.y > upper_y || ball.x > upper_x))
+		|| (at_location(get_position(), Location::LowerField) && (ball.y > upper_y || ball.x < lower_x))) {
+		spin(-35.0); // Robô gira no sentido horário
+	} else {
+		spin(35);//Robô gira no sentido anti-horário
+	}
+}
+
+void Attacker::uvf_to_goal(const Geometry::Point &ball) {
 	Vector ball_to_goal = their::goal::back::center - ball;
 
 	if(distance(ball, get_position()) > 0.03){
@@ -13,39 +26,16 @@ void Attacker::uvf_to_goal(Point ball) {
 	}
 }
 
-void Attacker::spin_shot(Point ball){
-	if (ball.y > get_position().y){
-		spin(35);//Robô gira no sentido anti-horário
-	}else{
-		if(ball.y == get_position().y) {
-		   if(ball.y > their::goal::front::center.y){
-			   spin(35);//Robô gira no sentido anti-horárioo
-		   }else{
-			   spin(-35);// Robô gira no sentido horário
-		   }
-		}else{
-			spin(-35);// Robô gira no sentido horário
-		}
-	}
+void Attacker::spin_shot(const Geometry::Point &ball) {
+	decide_spin_shot(ball);
 }
 
-void Attacker::crossing(Point ball){
-
-	if(ball.x > get_position().x) {
-		if(distance(ball, get_position()) < size) {
-			if (ball.y > get_position().y) {
-				spin(35);
-			}else{
-				spin(-35);
-			}
-		}else{
-			go_to(ball);
-		}
-	}else{
-		uvf_to_goal(ball);
-		//Se ele tiver a frente da bola, faz apenas o UVF,
-		//porém a condição tem que ser verificada pela estratégia antes de chamar o crossing,
-		//ele vai só entrar nesse else em caso de ser chamado em hora indevida.
+void Attacker::crossing(const Geometry::Point &ball){
+	if (distance(ball, get_position()) >= size) {
+		// longe da bola: vai até a bola
+		go_to(ball);
+	} else {
+		decide_spin_shot(ball);
 	}
 }
 
