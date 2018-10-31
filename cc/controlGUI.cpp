1bbe8593d42d0a1eaa84164bde44f8094406ab24
Daniel Silva <danielfaleirosilva@gmail.com>
2018-10-31 09:30:51 -0300

    Conexão automática do XBee (#154)
    * Conecta o xbee automaticamente ao abrir o programa (caso tenha só um)

--- cc/controlGUI.cpp
+++ cc/controlGUI.cpp
@@ -81,6 +81,7 @@ ControlGUI::ControlGUI() {
 	_create_status_frame();
 
 	_update_cb_serial();
+	auto_start_serial();
 	update_ack_interface();
 	// Conectar os sinais para o acontecimento dos eventos
 	button_PID_Test.signal_pressed().connect(sigc::mem_fun(*this, &ControlGUI::_PID_Test));
@@ -146,6 +147,12 @@ void ControlGUI::_robot_status() {
 			battery_bar[i].set_text("0%");
 			status_lb[i].set_text("Offline");
 		}
+	}
+}
+
+void ControlGUI::auto_start_serial() {
+	if (xbee_connections == 1) {
+		_start_serial();
 	}
 }
 
@@ -238,6 +245,7 @@ void ControlGUI::_update_cb_serial() {
 	messenger.stop_xbee();
 
 	cb_serial.remove_all();
+	xbee_connections = 0;
 
 	for (int i = 0; i < 256; ++i) {
 		std::string port = "/dev/ttyUSB";
@@ -248,8 +256,13 @@ void ControlGUI::_update_cb_serial() {
 			std::cout << port << std::endl;
 			cb_serial.append(port);
 			close(fd);
+			xbee_connections++;
 		}
 	}
+
+	// Caso tenha apenas algum dispositivo, atualizar a combo box
+	if (xbee_connections > 0)
+		cb_serial.set_active(0);
 
 	bt_Serial_Start.set_state(Gtk::STATE_NORMAL);
 	cb_serial.set_state(Gtk::STATE_NORMAL);
