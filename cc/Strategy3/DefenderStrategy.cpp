129aedd6fe50685979b8d97dd2107a123473487b
Bagre-de-Chapeu <fredscmorais@gmail.com>
2023-10-06 20:44:39 -0300

    strategy from the greek

--- cc/Strategy3/DefenderStrategy.cpp
+++ cc/Strategy3/DefenderStrategy.cpp
@@ -16,11 +16,64 @@ void DefenderStrategy::run_strategy(const Ball &ball, const Robot3 * attacker) {
 			wait_at_target(defender::back::upper_limit, ball.position);
     } else if (attacker && is_ball_est_ahead(df_limit, ball, -0.08) && !at_location(robot->get_position(), Location::AnyGoal)) {
 		// Bola na na área adversária
-		double pos_y = std::clamp(ball.position_in_seconds(1).y, defender::front::lower::wait_point.y, defender::front::upper::wait_point.y);
-		wait_at_target({attacker->get_position().x - robot->SIZE*5, pos_y}, ball.position);
-	} else if (at_location(robot->get_position(), Location::AnyGoal)) {
+		//std::vector<float> DefenderPoints = {Point{}, Point{}, Point{}};
+
+		if(at_location(robot->get_position(), Location::TheirField)){
+			//robot->go_to_and_stop({defender::back::upper_limit.x, ball.y});
+			//atacante + bola, defensor atras espelhado
+			//atk sem bola, defensor ponto + prox atras da bola 
+			double max_y = their::area::front::center.y;
+			double min_y = center::point.y;
+			double sizey = (max_y - min_y);
+
+			double max_x = field_height;
+			double min_x = center::point.x;
+			double sizex = (max_x - min_x);
+
+			int grid_x = 3;
+			int grid_y = 4;
+
+			double find_rounded_x = round(attacker->get_position().x * ((grid_x - 1)/ sizex));
+			double closest_x = find_rounded_x * (sizex / (grid_x - 1));
+
+			double find_rounded_y = round(attacker->get_position().y * ((grid_y - 1)/ sizey));
+			double closest_y = find_rounded_y * (sizey / (grid_y - 1));
+
+			Point closest = {closest_x , closest_y};
+
+			double distance_attacker_ball = distance(attacker->get_position() , ball.position);
+
+			if (distance_attacker_ball <= 0.3){
+
+				if(at_location(attacker->get_position(), field::Location::LowerField)){
+					closest_x = (find_rounded_x - 1) * (sizex / (grid_x - 1));
+					closest_y = (find_rounded_y + 2 ) * (sizey / (grid_y - 1));
+					closest = {closest_x , closest_y};
+					robot->go_to_and_stop(closest);
+				} else{
+					closest_x = (find_rounded_x - 1) * (sizex / (grid_x - 1));
+					closest_y = (find_rounded_y - 2 ) * (sizey / (grid_y - 1));
+					closest = {closest_x , closest_y};
+					robot->go_to_and_stop(closest);
+				}
+
+			} else {
+					find_rounded_x = round(ball.position.x * ((grid_x - 1)/ sizex));
+					find_rounded_y = round(ball.position.y * ((grid_y - 1)/ sizey));
+
+					closest_x = (find_rounded_x - 1) * (sizex / (grid_x - 1));
+					closest_y = (find_rounded_y - 2 ) * (sizey / (grid_y - 1));
+					closest = {closest_x , closest_y};
+					robot->go_to_and_stop(closest);
+
+			}
+		}
+	}
+		//double pos_y = std::clamp(ball.position_in_seconds(1).y, defender::front::lower::wait_point.y, defender::front::upper::wait_point.y);
+		//wait_at_target({attacker->get_position().x - robot->SIZE*5, pos_y}, ball.position);
+	else if (at_location(robot->get_position(), Location::AnyGoal)) {
 		exit_goal();
-	} else {
+	}else {
 		protect_goal(ball.position);
 	}
 }
